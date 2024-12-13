38a1dc47c0793ba3905b7e4894a8ae2d4995f130
FOV-RGT <Q1950088436@outlook.com>
2024-12-13 09:26:09 +0800

    修小bug

--- src/components/Plot2D.vue
+++ src/components/Plot2D.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 100%;height: 100%;" ref="canvas2D"></div>
+  <div style="width: 100%;height: 100%;user-select: none;" ref="canvas2D" ></div>
 </template>
 
 <script>
